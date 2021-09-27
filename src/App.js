597d59e2f1350d99443c600bd4e73c64b29bbec1
eldadtis <eldad.tishler@gmail.com>
2021-09-27 10:18:37 +0300

    add scrolltotop

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import ScrollToTop from "./components/ScrollToTop";
 import HomePage from "./pages/HomePage/HomePage";
 import AboutPage from "./pages/AboutPage/AboutPage";
 import ServicesPage from "./pages/ServicesPage/ServicesPage";
@@ -9,6 +10,7 @@ import ServicesPage from "./pages/ServicesPage/ServicesPage";
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Navbar />
       <Switch>
         <Route path="/" exact component={HomePage} />
